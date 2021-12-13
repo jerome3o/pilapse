582408d8467556af1f7f63a3edfcff8bc0825153
khi48 <kieranhitchcock28@gmail.com>
2021-12-13 20:00:03 +1300

    Implemented basic service

--- pilapse/legacy.py
+++ pilapse/legacy.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
 
-from picamera import PiCamera
-from time import sleep
+from typing import Union
+from pathlib import Path
 from datetime import datetime
+
+from picamera import PiCamera
+
+_camera = PiCamera()
+_camera.resolution = (3280, 2464)
+
+
+def take_picture(file_path: Union[str, Path]) -> None:
+    _camera.start_preview()
+    _camera.capture(str(file_path))
+    _camera.stop_preview()
 
 
 def main():
-    camera = PiCamera()
-    camera.resolution = (3280, 2464)
-    camera.start_preview()
-    sleep(1)
-
-    file_dir = './pictures'
-
-    now = datetime.now()
-
-    dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = 'test' + dt_string + '.jpg'
-
-    file_path = file_dir + file_name
-    camera.capture(file_path)
-    camera.stop_preview()
+    take_picture(Path("pictures") / f"test_{datetime.now():%Y-%m-%d_%H-%M-%S}.jpg")
 
 
 if __name__ == "__main__":
     import logging
+
     logging.basicConfig(level=logging.INFO)
     main()
